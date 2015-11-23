e4613deaf8eec1a87517106c87fb4daafe1f4fc0
Daniel Gruber <dgruber@univa.com>
2015-11-23 11:49:44 +0100

    DG: Logging

--- userList.go
+++ userList.go
@@ -99,6 +99,7 @@ func GetUserLists(userlist ...string) ([]UserList, error) {
 		}
 	}
 
+	cmd := exec.Command(qconf, "-su", csvUserList)
 	if out, errOut := cmd.CombinedOutput(); errOut == nil {
 		// empty line is the delimiter
 		uls := strings.Split(string(out), "\n\n")
